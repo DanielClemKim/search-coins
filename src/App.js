bd975e959d7d37882da6acf4890012be9945bed9
DanielClemKim <6323513@ed.tus.ac.jp>
2023-05-17 01:27:39 +0900

    style: fix design

--- src/App.js
+++ src/App.js
@@ -18,30 +18,50 @@ function App() {
   const onChange = (event) => {
     setSearchCoin(event.target.value);
   };
+  const onClick = () => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+      });
+  };
   return (
     <section className="section">
       <div className="container">
         <h1 className="title">Coins!</h1>
         <h2 className="subtitle">There's {coins.length} coins in this list</h2>
-        <a href="https://github.com/DanielClemKim/search-coins">
-          <button className="button is-pulled-right mb-3">
+        <div className="buttons">
+          <a
+            href="https://github.com/DanielClemKim/search-coins"
+            className="button"
+          >
             <span className="icon">
               <FontAwesomeIcon icon={faGithub} />
             </span>
             <span>GitHub</span>
-          </button>
-        </a>
+          </a>
+        </div>
         {loading ? (
           <strong>Loading...</strong>
         ) : (
-          <form className="control is-loading">
-            <input
-              className="input mb-3"
-              onChange={onChange}
-              type="text"
-              placeholder="What coins?"
-            />
-          </form>
+          <div className="columns is-centered">
+            <form className="column is-11 control">
+              <input
+                className="input mb-3"
+                onChange={onChange}
+                type="text"
+                placeholder="What coins?"
+              />
+            </form>
+            <div className="column buttons">
+              <button
+                onClick={onClick}
+                className="button px-5 ml-0 is-danger is-light"
+              >
+                <span>Refresh</span>
+              </button>
+            </div>
+          </div>
         )}
         <ul>
           {coins.map((coin) => {
@@ -52,7 +72,7 @@ function App() {
             const price = Math.round(coin.quotes.USD.price * 1000) / 1000;
             if (name.includes(nameOfSearch)) {
               return (
-                <div key={coin.id} className="columns mt-3">
+                <div key={coin.id} className="columns is-centered mt-3">
                   <li className="column box is-11 my-0 is-size-5">
                     <b>
                       {coin.name}({coin.symbol})
