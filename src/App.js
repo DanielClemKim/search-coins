7a0bb7a41c7dd0ddf340d01fa7af403aed22a9b6
DanielClemKim <6323513@ed.tus.ac.jp>
2023-05-16 19:46:24 +0900

    feat: add github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import "bulma/css/bulma.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -21,6 +23,14 @@ function App() {
       <div className="container">
         <h1 className="title">Coins!</h1>
         <h2 className="subtitle">There's {coins.length} coins in this list</h2>
+        <a href="https://github.com/DanielClemKim/search-coins">
+          <button class="button is-pulled-right mb-3">
+            <span class="icon">
+              <FontAwesomeIcon icon={faGithub} />
+            </span>
+            <span>GitHub</span>
+          </button>
+        </a>
         {loading ? (
           <strong>Loading...</strong>
         ) : (
@@ -40,7 +50,13 @@ function App() {
             if (nameOfCoin.includes(nameOfSearch)) {
               return (
                 <li className="box my-1" key={coin.id}>
-                  {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
+                  <b>
+                    {coin.name}({coin.symbol})
+                  </b>{" "}
+                  : ${coin.quotes.USD.price} USD
+                  <button className="button is-success is-small is-pulled-right px-5">
+                    Buy
+                  </button>
                 </li>
               );
             } else {
